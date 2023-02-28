7c70721bca54d38404340116bfb016f816e84482
Piletskii-Oleg <piletskii.oleg@gmail.com>
2023-02-28 17:45:01 +0300

    Implement list reversal

---
+++ ReverseList/ReverseList/Program.fs
@@ -0,0 +1,6 @@
+﻿let reverse_list ls =
+    let rec reverse acc ls_copy i =
+        if i = List.length ls then acc
+        else reverse (List.head ls_copy :: acc) (List.tail ls_copy) (i + 1)
+    reverse [] ls 0
+printfn $"%A{reverse_list [0; 1; 4; 64; 123; 3]}"
