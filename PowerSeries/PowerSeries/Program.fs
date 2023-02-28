9af4b31d8a763216fe6474ceb37bec71cc114985
Piletskii-Oleg <piletskii.oleg@gmail.com>
2023-02-28 18:02:36 +0300

    Implement power series

---
+++ PowerSeries/PowerSeries/Program.fs
@@ -0,0 +1,10 @@
+﻿let power_series n m =
+    let rec power_two acc power =
+        if power = n + m then acc
+        else if power > (n + m) then power_two (acc / 2L) (power - 1)
+        else power_two (acc * acc) (power + power)
+    let rec create_list acc element i =
+        if i = m + 1 then acc
+        else create_list (element :: acc) (element / 2L) (i + 1)
+    create_list [] (power_two 2L 1) 0
+printfn $"%A{power_series 2 4}"
