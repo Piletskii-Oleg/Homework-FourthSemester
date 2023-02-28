5833c82bedc4822b3b1df7db8773a4fd18c7335e
Piletskii-Oleg <piletskii.oleg@gmail.com>
2023-02-28 15:52:23 +0300

    Implement Fibonacci numbers

---
+++ Fibonacci/Fibonacci/Program.fs
@@ -0,0 +1,5 @@
+﻿let rec fibonacci n =
+    let rec fibAdditional twoBack oneBack i =
+        if i = n then twoBack
+        else fibAdditional oneBack (twoBack + oneBack) (i + 1)
+    fibAdditional 0 1 0
