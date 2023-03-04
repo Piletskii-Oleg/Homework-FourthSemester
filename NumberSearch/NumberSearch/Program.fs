62d46a5757b5b15117930a3eb7b72338066d173a
Piletskii-Oleg <piletskii.oleg@gmail.com>
2023-03-04 16:22:06 +0300

    Use pattern matching

--- NumberSearch/NumberSearch/Program.fs
+++ NumberSearch/NumberSearch/Program.fs
@@ -1,7 +1,8 @@
-﻿let find_element ls element =
-    let rec check_element ls_copy i = 
-       if i = List.length ls then -1
-       else if (List.head ls_copy = element) then i
-       else check_element (List.tail ls_copy) (i + 1)
-    check_element ls 0
-printfn $"%A{find_element [1; 3; 5] 4}, %A{find_element [1; 3; 5] 5}"
+﻿let find_element element ls =
+    let rec check_element i ls_copy =
+       match i with
+       | _ when i = List.length ls -> -1
+       | _ when List.head ls_copy = element -> i
+       | _ -> check_element (i + 1) (List.tail ls_copy)
+    check_element 0 ls
+printfn $"%A{find_element 4 [1; 3; 5]}, %A{find_element 5 [1; 3; 5]}"
