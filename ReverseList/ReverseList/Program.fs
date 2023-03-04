24f1c8abd92e2dbe91a3d9c7102e3a68d4d2f947
Piletskii-Oleg <piletskii.oleg@gmail.com>
2023-03-04 19:50:05 +0300

    Use pattern matching

--- ReverseList/ReverseList/Program.fs
+++ ReverseList/ReverseList/Program.fs
@@ -1,6 +1,7 @@
 ﻿let reverse_list ls =
-    let rec reverse acc ls_copy i =
-        if i = List.length ls then acc
-        else reverse (List.head ls_copy :: acc) (List.tail ls_copy) (i + 1)
-    reverse [] ls 0
-printfn $"%A{reverse_list [0; 1; 4; 64; 123; 3]}"
+    let rec reverse ls_copy acc =
+        match ls_copy with
+        | head :: tail -> reverse tail (head :: acc)
+        | [] -> acc
+    reverse ls []
+printfn $"%A{reverse_list [3; 5]}"
