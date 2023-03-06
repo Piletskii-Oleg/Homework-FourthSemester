1ac4917cf958638716d2427b6baefae37c8a5fb9
Piletskii-Oleg <piletskii.oleg@gmail.com>
2023-03-06 21:44:35 +0300

    Add tests and change program a little

--- NumberSearch/NumberSearch/Program.fs
+++ NumberSearch/NumberSearch/Program.fs
@@ -1,8 +1,12 @@
-﻿let find_element element ls =
+﻿module NumberSearch
+
+/// Returns index of the element in the list;
+/// returns -1 if the element is not present in the list.
+let find_element element ls =
     let rec check_element i ls_copy =
-       match i with
-       | _ when i = List.length ls -> -1
-       | _ when List.head ls_copy = element -> i
-       | _ -> check_element (i + 1) (List.tail ls_copy)
+       match ls_copy with
+       | [] -> -1
+       | head :: _ when head = element -> i
+       | _ :: tail -> check_element (i + 1) tail
     check_element 0 ls
 printfn $"%A{find_element 4 [1; 3; 5]}, %A{find_element 5 [1; 3; 5]}"
