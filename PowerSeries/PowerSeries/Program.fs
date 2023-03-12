4cc28efd16f3ec6d873d0070279dcd9de8651a20
Piletskii-Oleg <piletskii.oleg@gmail.com>
2023-03-12 13:01:43 +0300

    Add CI and tests

--- PowerSeries/PowerSeries/Program.fs
+++ PowerSeries/PowerSeries/Program.fs
@@ -1,4 +1,8 @@
-﻿let power_series n m =
+﻿module PowerSeries
+
+/// Creates a list of consecutive powers of 2,
+/// starting from 2^n and ending with 2^(n + m)
+let power_series n m =
     let rec power_two power acc =
         match power with
         | _ when power = n -> acc
@@ -6,7 +10,7 @@
         | _ -> power_two (power + power) (acc * acc)
     let rec create_list element i acc =
         match i with
-        | _ when i = m + 1 -> acc
+        | _ when i >= m + 1 -> acc
         | _ -> create_list (element * 2L) (i + 1) (element :: acc)
     List.rev (create_list (power_two 1 2L) 0 [])
 printfn $"%A{power_series 2 4}"
