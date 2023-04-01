00fbd60a92fde213b4f7c9d37acde5f6494e0343
Piletskii-Oleg <piletskii.oleg@gmail.com>
2023-04-01 18:04:53 +0300

    Implement functionality

---
+++ PhoneBook/PhoneBook/PhoneBook.fs
@@ -0,0 +1,52 @@
+﻿module PhoneBook
+
+open System.IO
+
+type PhoneRecord =
+    { Phone: string;
+      Name: string }
+    
+let addRecord phoneBook value : PhoneRecord list =
+    value :: phoneBook
+   
+let parseFile (stream: Stream) =
+    use reader = new StreamReader(stream)
+    let wholeString = reader.ReadToEnd()
+    
+    wholeString.Split "\n"
+    |> Seq.map (fun (string: string) -> string.Split())
+    |> Seq.map (fun [|phone; name|] -> {Phone = phone; Name = name})
+    |> Seq.toList
+    
+let findByName phoneBook value =
+    let rec iterate listCopy =
+        match listCopy with
+        | [] -> "not found"
+        | head :: tail ->
+            match head with
+            | {Phone = phone; Name = name} when name = value -> phone
+            | _ -> iterate tail
+    iterate phoneBook
+    
+let findByPhone phoneBook value =
+    let rec iterate listCopy =
+        match listCopy with
+        | [] -> "not found"
+        | head :: tail ->
+            match head with
+            | {Phone = phone; Name = name} when phone = value -> name
+            | _ -> iterate tail
+    iterate phoneBook
+    
+let rec print phoneBook =
+    match phoneBook with
+    | [] -> printfn ""
+    | head :: tail -> printfn $"%s{head.Name} - %s{head.Phone}"; print tail
+    
+let saveToFile phoneBook (path: string) =
+    use writer = new StreamWriter(path)
+    let rec writeLine list =
+        match list with
+        | [] -> writer.Close
+        | head :: tail -> writer.WriteLine $"%s{head.Name} %s{head.Phone}"; writeLine tail
+    writeLine phoneBook
--- PhoneBook/PhoneBook/Program.fs
+++ PhoneBook/PhoneBook/Program.fs
@@ -1,2 +1,12 @@
-﻿namespace PhoneBook
+﻿open System.IO
+open PhoneBook
 
+let options =
+    "0 - quit\n" + "1 - read from file\n" + "2 - add record\n" + "3 - find name by phone\n"
+    + "4 - find phone by name\n" + "5 - print everything\n" + "6 - save current data to file"
+
+
+[<EntryPoint>]
+let main args =
+   printf $"%s{options}"
+   0
