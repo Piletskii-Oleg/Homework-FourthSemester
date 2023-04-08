ca0d0983b51cd01a70fc633728b4a0e698d16cdc
Piletskii-Oleg <piletskii.oleg@gmail.com>
2023-04-08 16:29:00 +0300

    Fantomas fixes

--- PhoneBook/PhoneBook/PhoneBook.fs
+++ PhoneBook/PhoneBook/PhoneBook.fs
@@ -3,35 +3,35 @@
 open System.IO
 
 /// A record that stores name of a person and his phone number
-type PhoneRecord =
-    { Phone: string;
-      Name: string }
-    
+type PhoneRecord = { Phone: string; Name: string }
+
 /// Adds a record to the phone book
-let addRecord value phoneBook : PhoneRecord list =
-    value :: phoneBook
-   
+let addRecord value phoneBook : PhoneRecord list = value :: phoneBook
+
 /// Gets a phone book from the file
 let parseFile (path: string) =
     use reader = new StreamReader(path)
     let wholeString = reader.ReadToEnd()
-    
+
     wholeString.Split "\n"
     |> Seq.map (fun (string: string) -> string.Split())
-    |> Seq.map (fun element -> {Name = element[0]; Phone = element[1]})
+    |> Seq.map (fun element ->
+        { Name = element[0]
+          Phone = element[1] })
     |> Seq.toList
-    
+
 /// Finds phone number of a person by his name
-let findByName value phoneBook=
+let findByName value phoneBook =
     let rec iterate listCopy =
         match listCopy with
         | [] -> "not found"
         | head :: tail ->
             match head with
-            | {Phone = phone; Name = name} when name = value -> phone
+            | { Phone = phone; Name = name } when name = value -> phone
             | _ -> iterate tail
+
     iterate phoneBook
-    
+
 /// Finds name of a person by his phone number
 let findByPhone value phoneBook =
     let rec iterate listCopy =
@@ -39,18 +39,20 @@ let findByPhone value phoneBook =
         | [] -> "not found"
         | head :: tail ->
             match head with
-            | {Phone = phone; Name = name} when phone = value -> name
+            | { Phone = phone; Name = name } when phone = value -> name
             | _ -> iterate tail
+
     iterate phoneBook
-    
+
 /// Returns a string composed of PhoneRecords
 let getPhoneBookString phoneBook =
-    let rec getPhoneBookLine phoneBookString phoneBookTail = 
+    let rec getPhoneBookLine phoneBookString phoneBookTail =
         match phoneBookTail with
         | [] -> phoneBookString
         | head :: tail -> getPhoneBookLine (phoneBookString + $"%s{head.Name} %s{head.Phone}\n") tail
+
     getPhoneBookLine "" phoneBook
-    
+
 /// Saves a phone book to the file
 let saveToFile (path: string) phoneBook =
     use writer = new StreamWriter(path)
--- PhoneBook/PhoneBook/Program.fs
+++ PhoneBook/PhoneBook/Program.fs
@@ -1,49 +1,54 @@
 ﻿let options =
-    "0 - quit\n" + "1 - read from file\n" + "2 - add record\n" + "3 - find name by phone\n"
-    + "4 - find phone by name\n" + "5 - print everything\n" + "6 - save current data to file\n"
+    "\"quit\" - quit\n"
+    + "\"read\" - read from file\n"
+    + "\"add\" - add record\n"
+    + "\"findPhone\" - find name by phone\n"
+    + "\"findName\" - find phone by name\n"
+    + "\"print\" - print everything\n"
+    + "\"save\" - save current data to file\n"
 
 /// Processes user's input according to the options
 let rec processInput phoneBook =
-   printf "Please input a command: "
-   let input = System.Int32.Parse(System.Console.ReadLine())
-   match input with
-   | 0 -> ()
-   | 1 ->
-       printf "Please enter full path to the file: "
-       let path = System.Console.ReadLine()
-       processInput (PhoneBook.parseFile path)
-   | 2 ->
-       printfn "Please enter the name and the phone that should be added."
-       printf "Name: "
-       let name = System.Console.ReadLine()
-       printf "Phone: "
-       let phone = System.Console.ReadLine()
-       processInput (PhoneBook.addRecord {Name = name; Phone = phone} phoneBook)
-   | 3 ->
-       printf "Please enter the name by which the phone should be found: "
-       let name = System.Console.ReadLine()
-       printfn $"%s{PhoneBook.findByName name phoneBook}"
-       processInput phoneBook
-   | 4 ->
-       printf "Please enter the phone by which the name should be found: "
-       let phone = System.Console.ReadLine()
-       printfn $"%s{PhoneBook.findByPhone phone phoneBook}"
-       processInput phoneBook
-   | 5 ->
-       printfn $"%s{PhoneBook.getPhoneBookString phoneBook}"
-       processInput phoneBook
-   | 6 ->
-       printf "Please enter full path to the file: "
-       let path = System.Console.ReadLine()
-       PhoneBook.saveToFile path phoneBook
-       processInput phoneBook
-   | _ ->
-       printfn $"%s{options}"
-       processInput phoneBook
-       
+    printf "Please input a command: "
+    let input = System.Console.ReadLine()
 
+    match input with
+    | "quit" -> ()
+    | "read" ->
+        printf "Please enter full path to the file: "
+        let path = System.Console.ReadLine()
+        processInput (PhoneBook.parseFile path)
+    | "add" ->
+        printfn "Please enter the name and the phone that should be added."
+        printf "Name: "
+        let name = System.Console.ReadLine()
+        printf "Phone: "
+        let phone = System.Console.ReadLine()
+        processInput (PhoneBook.addRecord { Name = name; Phone = phone } phoneBook)
+    | "findPhone" ->
+        printf "Please enter the name by which the phone should be found: "
+        let name = System.Console.ReadLine()
+        printfn $"%s{PhoneBook.findByName name phoneBook}"
+        processInput phoneBook
+    | "findName" ->
+        printf "Please enter the phone by which the name should be found: "
+        let phone = System.Console.ReadLine()
+        printfn $"%s{PhoneBook.findByPhone phone phoneBook}"
+        processInput phoneBook
+    | "print" ->
+        printfn $"%s{PhoneBook.getPhoneBookString phoneBook}"
+        processInput phoneBook
+    | "save" ->
+        printf "Please enter full path to the file: "
+        let path = System.Console.ReadLine()
+        PhoneBook.saveToFile path phoneBook
+        processInput phoneBook
+    | _ ->
+        printfn $"%s{options}"
+        processInput phoneBook
+        
 [<EntryPoint>]
 let main _ =
-   printfn $"%s{options}"
-   processInput []
-   0
+    printfn $"%s{options}"
+    processInput []
+    0
