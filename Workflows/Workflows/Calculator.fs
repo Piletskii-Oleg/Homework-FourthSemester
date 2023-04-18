77c355cfc035e40df0b9451c1024162b874f3f7b
Piletskii-Oleg <piletskii.oleg@gmail.com>
2023-04-18 12:11:57 +0300

    Implement workflows with tests

---
+++ Workflows/Workflows/Calculator.fs
@@ -0,0 +1,10 @@
+﻿module Calculator
+
+type StringCalculator() =
+    member _.Bind(x: string, f) =
+        let isNumber, maybeNumber = System.Int32.TryParse(x)
+        match isNumber with
+        | true -> f maybeNumber
+        | false -> None
+
+    member _.Return(x) = Some x
---
+++ Workflows/Workflows/Rounding.fs
@@ -0,0 +1,8 @@
+﻿module Rounding
+
+type Rounding(precision: int) =
+    member _.Bind(x, f) =
+        let rounded = System.Double.Round(x, precision)
+        f rounded
+        
+    member _.Return(x) = System.Double.Round(x, precision)
