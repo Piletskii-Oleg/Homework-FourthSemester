e32751e2667fe66c6e70ace660f56a30be6c43b1
Piletskii-Oleg <piletskii.oleg@gmail.com>
2023-04-21 12:50:07 +0300

    Update SafeStackTests.fs

--- Test1/Test1.Tests/SafeStackTests.fs
+++ Test1/Test1.Tests/SafeStackTests.fs
@@ -30,12 +30,12 @@ let ``Multi-threaded safe Push should work correctly`` () =
 let ``Unsafe Push should not work correctly`` () =
     let stack = SafeStack([ 2; 3 ])
     let asyncPush element = async { return stack.UnsafePush element }
-    List.init 10000 asyncPush
+    List.init 1000000 asyncPush
     |> Async.Parallel
     |> Async.RunSynchronously
     |> ignore
     
-    stack.Stack.Length |> should not' (equal 10002)
+    stack.Stack.Length |> should not' (equal 1000002)
     
 [<Test>]
 let ``Safe TryPop should work correctly`` () =  
@@ -51,9 +51,9 @@ let ``Safe TryPop should work correctly`` () =
     
 [<Test>]
 let ``Unsafe TryPop should not work correctly`` () =
-    let stack = SafeStack(List.init 10000 id)
+    let stack = SafeStack(List.init 1000000 id)
     let asyncTryPop _ = async { return stack.UnsafeTryPop() }
-    List.init 10000 asyncTryPop
+    List.init 1000000 asyncTryPop
     |> Async.Parallel
     |> Async.RunSynchronously
     |> ignore
