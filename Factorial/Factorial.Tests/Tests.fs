3c566fa524813bd6dd1a0eedbbbbe65089a6f7d4
Piletskii-Oleg <piletskii.oleg@gmail.com>
2023-05-17 20:24:55 +0300

    add negative numbers case

--- Factorial/Factorial.Tests/Tests.fs
+++ Factorial/Factorial.Tests/Tests.fs
@@ -8,3 +8,10 @@ let ``Factorial should give correct results`` () =
     factorial 0 |> should equal 1
     factorial 5 |> should equal 120
     factorial 15 |> should equal 1307674368000L
+    
+[<Test>]
+let ``Factorial of negative numbers should return -1`` () =
+    List.init 10000 (fun x -> -x)
+    |> List.tail // first element is 0
+    |> List.map factorial
+    |> List.iter (fun x -> x |> should equal -1L)
--- Factorial/Factorial/Program.fs
+++ Factorial/Factorial/Program.fs
@@ -1,9 +1,11 @@
 ﻿module Factorial
 
 let factorial x =
-    let rec loop i acc =
-        match i with
-        | 0L | 1L -> acc
-        | _ -> loop (i - 1L) (acc * i)
-    loop x 1
-printfn $"%d{factorial 5L}"
+    if x < 0 then -1L
+    else
+         let rec loop i acc =
+            match i with
+            | 0L | 1L -> acc
+            | _ -> loop (i - 1L) (acc * i)
+         loop x 1L
+printfn $"%d{factorial 5}"
